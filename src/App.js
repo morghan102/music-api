89f458dc3eff22e3f39d059fece537d984df9625
Morghan <Morghan102@gmail.com>
2021-10-05 12:28:01 -0700

    trying to fix cors error, failed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
-import { useState } from 'react'
-import { apikey, url } from './urls' // not sure how i'd do this
-import styles from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+import { useEffect, useState } from 'react'
+import { apikey, url } from './urls';
+import 'bootstrap/dist/css/bootstrap.css'; //i think i dont use
 import { Button, Col, Container, Row, DropdownButton, Dropdown, Form } from 'react-bootstrap';
 import { Label } from 'reactstrap';
+
 
 // use react-bootstrap for making page mobile friendly (remem this is where its really useful)
 function App() {
   const [lyrics, setLyrics] = useState('');
   const [artist, setArtist] = useState('');
   const [song, setSong] = useState('');
+
 
   const getLyrics = async () => {
     try { //not sure if this is being used correctly here
@@ -23,7 +24,28 @@ function App() {
       alert(err)
     }
   }
-  // getLyrics.catch(alert)
+
+  // trying to fix the cors error. also reference server.js
+  // const [data, setData] = useState('');
+
+  // useEffect(() => {
+  //   callBackendAPI()
+  //   .then(res => setData(res.express))
+  //   .catch(err => console.log(err))    
+  // })
+  // const callBackendAPI = async() => {
+  //   const response = await fetch('/express_backend');
+  //   // console.log(response)
+  //   const body = await response.json();
+  //   const almostThere = data.message.body.lyrics.lyrics_body;
+  //   setLyrics(almostThere.substring(0, almostThere.length - 69))
+  //   console.log(lyrics)
+
+  //   if (response.status !== 200) {
+  //     throw Error(body.message) 
+  //   }
+  //   return lyrics;
+  // }
 
 
   function projectExplanation() {
@@ -57,42 +79,42 @@ function App() {
   function MusicForm() {
     return (
       // <Container fluid>
-        <Form>
-          <Row className="justify-content-sm-center">
-            <Col sm="auto">
-              <Label> 
-                Song title:   
-                <input
-                  type="text"
-                  value={song}
-                  onChange={e => setSong(e.target.value)} //e is this whole big object
-                />
-              </Label>
-            </Col>
-            {/* break here */}
-            <Col sm="auto">
-              <Label>
-                Artist:
-                <input
-                  type="text"
-                  value={artist}
-                  onChange={e => setArtist(e.target.value)}
-                />
-              </Label>
-            </Col>
-            <Col sm="auto">
+      <Form>
+        <Row className="justify-content-sm-center">
+          <Col sm="auto">
+            <Label>
+              Song title:
+              <input
+                type="text"
+                value={song}
+                onChange={e => setSong(e.target.value)} //e is this whole big object
+              />
+            </Label>
+          </Col>
+          {/* break here */}
+          <Col sm="auto">
+            <Label>
+              Artist:
+              <input
+                type="text"
+                value={artist}
+                onChange={e => setArtist(e.target.value)}
+              />
+            </Label>
+          </Col>
+          <Col sm="auto">
             <Form.Select>
               <option value="0">Select preferred way of displaying the data</option>
               <option value="1">1st way of displaying the data</option>
               <option value="2">2nd way of displaying the data</option>
               <option value="3">3rd way of displaying the data</option>
             </Form.Select>
-            </Col>
-          </Row>
-          <Button onClick={getLyrics}>Get Those Lyrics</Button> {/* this will handle submission of the music form */}
+          </Col>
+        </Row>
+        <Button onClick={getLyrics}>Get Those Lyrics</Button> {/* this will handle submission of the music form */}
 
-          {/* <input type="submit" value="Submit" /> replace(" ", '%20') */}
-        </Form>
+        {/* <input type="submit" value="Submit" /> replace(" ", '%20') */}
+      </Form>
       // </Container>
     )
   }
